6f65af5ff7652b0f33dfac756734f6a3f4f562e2
l624609074 <huang8351067>
2015-06-29 15:14:02 +0800

    全部支付宝开发完成

--- Application/Appmaka/Controller/SubOrderController.class.php
+++ Application/Appmaka/Controller/SubOrderController.class.php
@@ -2,45 +2,78 @@
 	namespace Appmaka\Controller;
 	use Think\Controller;
 	Class SubOrderController extends Controller{
-			//该类是前台提交订单类
 			function submit(){
-				
+						
 				//获取 自定义的 验证规则
 				$verify=D("Verify"); 
 
 				if(!$verify->create()){
-				$this->error($verify->getError());	
+					$this->error($verify->getError());	
 			
-				} 
-				$rules=array(
-						  
-						  array('status','0'), 
-						
-				);
-				$order=M("order");
-				if($order->create()){        //采用自动完成
-					$randNum=rand(0,9999999999999);
+				}    
+				 
+				//验证通过了。现在插入数据
+			
+					$randNum=rand(0,9999999999999);         //生成  订单号的操作
 					$time=time();
 					$str=$randNum.$time;
-					$orderid=substr(md5($str),1,12);
-					$order->ordertime=NOW_TIME;
-					$order->orderid=$orderid;
-					$id=$order->auto($rules)->add();
-					if($id){
-						$this->success("您的订单已经提交\n您的订单号是【{$orderid}】\n我们将在1个工作日和您联系！",'',4);
+					$ordernum=substr(md5($str),1,12);
+				
+				//共有两个操作 1.用户联系方式  2.购买信息的操作
+				
+				//1.用户联系方式的操作 
+				
+				$contact=M("contact");
+				$_POST["ordernum"]=$ordernum;
+				if($contact->field("name,phone,address,word,ordernum")->create()){
+					
+						if(!$contactId=$contact->add()){                   //$contactId 用于下面的操作，如果 购买的产品信息插入失败，则可以通过该 id 删除上面插入的用户数据
+							
 						
-						
-					}	
+							$this->error("提交订单失败，请联系管理员1！");
+							
+						}
 					
 				}else{
 					
-					$this->error("提交订单失败，请联系管理员！");
-					
+					$this->error("提交订单失败，请联系管理员2！");
 				}
 				
+			
+				//2,插入购买产品的信息
+				
+
+				//插入到用户购买的信息表中，$key 为产品的id ，num_two 为该产品 的数量
+										$num=1;
+										$productid=I("post.productid");
+										$payment=I("post.payment");
+										$word=I("post.word");
+										$client=I("post.client");
+										$products=M("products");
+										$whereProduct=array("id"=>$productid);
+										$productPrice=$products->field("price")->where($whereProduct)->find();   
+										$price=$productPrice["price"];
+										
+										$total=$price*$num;    //算出当前这件 总价	
+										$time=NOW_TIME;
+										$sql="insert into tp_order (payment,productid,num,total,word,ordernum,ordertime,client) values('{$payment}','{$productid}','{$num}','{$total}','{$word}','{$ordernum}','{$time}','{$client}')";
+										$order=M("order");
+
+										if(!$order->execute($sql)){
+											
+											$where["id"]=$contactId;
+											$contact->where($where)->delete();
+											
+												$this->error("提交订单失败，请联系管理员3！");
+														
+											
+										}
+					
+					$this->success("提交订单成功，您的订单号是【{$ordernum}】,我们将在1个工作日内和您联系！",'',4);
 				
 				
 			}
+		
 		
 		
 		
